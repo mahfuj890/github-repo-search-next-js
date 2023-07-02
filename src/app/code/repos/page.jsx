6d6cb7cbae058669a38bdb738dda38cf87f16ed1
mahfuj890 <mahfujali490@gmail.com>
2023-07-02 12:58:10 +0600

    show repos

--- src/app/code/repos/page.jsx
+++ src/app/code/repos/page.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
 
-function ReposPage() {
-  return <div>ReposPage</div>;
+const getRepos = async () => {
+  const fetchRepos = await fetch(
+    "https://api.github.com/users/mahfuj890/repos"
+  );
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetchRepos.json();
+  return response;
+};
+
+async function ReposPage() {
+  const repos = await getRepos();
+
+  return (
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ReposPage;
